02871744583678ac2a322bf1ed93e2fa762d6085
Miensoap <miensop51@gmail.com>
2024-05-08 15:38:28 +0900

    ✨ feature1 : db의 모든 테이블 더미 데이터 생성

---
+++ be/issue_tracker/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into USERS (id, password, name, created_at)
+values ('test2', 'test123', '테스트유저2', '2024-05-08-00:00:00');
+
+insert into ISSUE (user_id, title, comment, created_at)
+VALUES ('test2', '제목', '내용', '2024-05-08-00:00:00');
+
+insert into LABEL (name, description, created_at, color)
+VALUES ('todo', '오늘 할 일', '2024-05-08-00:00:00', '#3DDCFF');
+
+insert into ISSUE_LABEL (issue_id, label_id) VALUES (1, 1);
+
+insert into MILESTONE (name, description, created_at, deadline)
+VALUES ('feat_1', '피쳐원', '2024-05-08-00:00:00', '2024-05-18-00:00:00');
+
+insert into ISSUE_MILESTONE (issue_id, milestone_id) VALUES (1,1);
