733b6f5e9fc12f22cc71755f674058b33bd291e6
Miensoap <miensop51@gmail.com>
2024-05-10 16:40:42 +0900

    🔧 테스트 관련 세팅 수정
    테스트 기본 프로파일을 'test' 로 설정

--- be/issue_tracker/src/test/java/team1/issue_tracker/IssueTrackerApplicationTests.java
+++ be/issue_tracker/src/test/java/team1/issue_tracker/IssueTrackerApplicationTests.java
@@ -1,13 +1,24 @@
 package team1.issue_tracker;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import team1.issue_tracker.Issue.IssueRepository;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class IssueTrackerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    IssueRepository issueRepository;
 
+    @Test
+    @DisplayName("MySql DB 연결에 성공한다")
+    void testDbConnection(){
+        assertDoesNotThrow(() -> issueRepository.findAll());
+    }
 }
--- be/issue_tracker/src/test/java/team1/issue_tracker/label/LabelRepositoryTest.java
+++ be/issue_tracker/src/test/java/team1/issue_tracker/label/LabelRepositoryTest.java
@@ -6,31 +6,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJdbcTest
-@ActiveProfiles("test")
 public class LabelRepositoryTest {
 
     @Autowired
     LabelRepository labelRepository;
 
+    @Test
+    void idAutoGenerate() {
+        Label label = Label.builder()
+                .name("이름")
+                .description("설명")
+                .color("#000000")
+                .createdAt(Timestamp.valueOf(LocalDateTime.now()))
+                .build();
+
+        assertThat(label.getId()).isNull();
+
+        Label saved = labelRepository.save(label);
+        assertThat(saved.getId()).isNotNull();
+    }
+
     @DisplayName("라벨을 저장하고 조회할 수 있다")
     @Test
     void saveAndFindById() {
         // given
-        Label label = new Label("이름", "설명", "#000000");
+        Label label = Label.builder()
+                .name("이름")
+                .description("설명")
+                .color("#000000")
+                .createdAt(Timestamp.valueOf(LocalDateTime.now()))
+                .build();
 
         // when
         labelRepository.save(label);
+        System.out.println(labelRepository.count());
         Optional<Label> byId = labelRepository.findById(1L);
 
         //then
-        assertThat(byId.isPresent()).isTrue();
+//        assertThat(byId.isPresent()).isTrue();
         assertThat(byId.get().getName()).isEqualTo("이름");
         assertThat(byId.get().getDescription()).isEqualTo("설명");
         assertThat(byId.get().getColor()).isEqualTo("#000000");
