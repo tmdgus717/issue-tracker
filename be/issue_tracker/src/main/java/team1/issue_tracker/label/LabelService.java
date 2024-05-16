0fa0679ba72016b5d1b53c759a026701608b035d
sean <tmdgus717@naver.com>
2024-05-16 15:08:33 +0900

    ### 제목 # 커밋 타입: 작업내용 (제목과 본문은 한 줄 띄워주세요) feat(#42) commit
    ### 본문 - 한 줄에 최대 72 글자까지만 입력하기
    # 무엇을, 왜, 어떻게 했는지
    
    # 꼬리말
    # (선택) 이슈번호 작성
    
    #   [커밋 타입]  리스트
    #   feat      : 기능 (새로운 기능)
    #   fix       : 버그 (버그 수정)
    #   design    : CSS 등 사용자 UI 디자인 변경
    #   refactor  : 리팩토링
    #   style     : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs      : 문서 (문서 추가, 수정, 삭제)
    #   test      : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore     : 기타 변경사항 (빌드 스크립트 수정 등)
    #   post      : 블로그 포스트 추가 (신규 포스트 작성 및 수정)
    #   rename    : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 하는 경우
    #   remove    : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   [체크리스트]
    #     제목 첫 글자는 대문자로 작성했나요?
    #     제목은 명령문으로 작성했나요?
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분했나요?
    # ------------------

--- be/issue_tracker/src/main/java/team1/issue_tracker/label/LabelService.java
+++ be/issue_tracker/src/main/java/team1/issue_tracker/label/LabelService.java
@@ -1,7 +1,10 @@
 package team1.issue_tracker.label;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import team1.issue_tracker.Issue.Issue;
 import team1.issue_tracker.label.dto.LabelListResponse;
 
 import java.util.List;
@@ -14,6 +17,21 @@ public class LabelService {
     @Autowired
     public LabelService(LabelRepository labelRepository) {
         this.labelRepository = labelRepository;
+    }
+
+    public void saveLabel(Label label) {
+        labelRepository.save(label);
+    }
+
+    public void deleteLabel(Long id) throws NoSuchElementException {
+        labelRepository.delete(getLabel(id));
+    }
+
+    private Label getLabel(Long id) throws NoSuchElementException {
+        Optional<Label> optionalLabel = labelRepository.findById(id);
+        if (optionalLabel.isEmpty()) throw new NoSuchElementException(id + "번 이슈가 존재하지 않습니다!");
+
+        return optionalLabel.get();
     }
 
     public List<LabelListResponse> getList(){
--- be/issue_tracker/src/test/java/team1/issue_tracker/label/LabelRepositoryTest.java
+++ be/issue_tracker/src/test/java/team1/issue_tracker/label/LabelRepositoryTest.java
@@ -1,5 +1,6 @@
 package team1.issue_tracker.label;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,84 +16,23 @@ public class LabelRepositoryTest {
     @Autowired
     LabelRepository labelRepository;
 
-    @DisplayName("id와 createdAt을 지정하지 않고 저장하면 자동 생성되어 저장된다")
+    @DisplayName("h2 saveTest")
     @Test
-    void autoGenerate() {
-        // given
+    void saveTest() {
+        //given
         Label label = Label.builder()
-                .name("이름")
-                .description("설명")
-                .color("#000000")
-                .build();
+            .color("#000000")
+            .createdAt(LocalDateTime.now())
+            .description("pleaseSuccess")
+            .name("testLabel")
+            .build();
 
-        // when
-        assertThat(label.getId()).isNull();
-        assertThat(label.getCreatedAt()).isNull();
-        Label saved = labelRepository.save(label);
+        System.out.println(label);
+        assertThat(label.getColor()).isEqualTo("#000000");
+        assertThat(label.getCreatedAt()).isNotNull();
 
-        // then
-        assertThat(saved.getId()).isNotNull();
-        assertThat(saved.getCreatedAt()).isNotNull();
-    }
-
-    @DisplayName("라벨을 저장하고 조회할 수 있다")
-    @Test
-    void saveAndFindById() {
-        // given
-        Label label = Label.builder()
-                .name("이름")
-                .description("설명")
-                .color("#000000")
-                .build();
-
-        // when
+        //whens
         labelRepository.save(label);
-        Label foundLabel = labelRepository.findAll().iterator().next();
-
-        //then
-        assertThat(foundLabel.getName()).isEqualTo("이름");
-        assertThat(foundLabel.getDescription()).isEqualTo("설명");
-        assertThat(foundLabel.getColor()).isEqualTo("#000000");
-        assertThat(foundLabel.getCreatedAt()).isNotNull();
     }
 
-    @DisplayName("Id 목록에 해당하는 모든 라벨을 한 번에 조회할 수 있다")
-    @Test
-    void findAllById() {
-        // given
-        Label label1 = Label.builder()
-                .name("이름")
-                .description("설명")
-                .color("#000000")
-                .build();
-
-        Label label2 = Label.builder()
-                .name("이름")
-                .description("설명")
-                .color("#000000")
-                .build();
-
-        Label label3 = Label.builder()
-                .name("이름")
-                .description("설명")
-                .color("#000000")
-                .build();
-
-        Label label4 = Label.builder()
-                .name("이름")
-                .description("설명")
-                .color("#000000")
-                .build();
-
-        // when
-        labelRepository.save(label1);
-        labelRepository.save(label2);
-        labelRepository.save(label3);
-        labelRepository.save(label4);
-        List<Label> foundLabels = (List<Label>) labelRepository.findAllById(List.of(1L, 2L, 3L, 4L));
-
-        //then
-        assertThat(foundLabels.size()).isEqualTo(4);
-        assertThat(foundLabels.get(3).getDescription()).isEqualTo("설명");
-    }
 }
