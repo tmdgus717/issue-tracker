a37671b0cef573c1174048c67efdae9bf85e994e
Miensoap <miensop51@gmail.com>
2024-05-16 12:11:46 +0900

    feat(#46) 회원 가입 API 구현
    중복 확인 API 구현

--- be/issue_tracker/src/main/java/team1/issue_tracker/user/UserService.java
+++ be/issue_tracker/src/main/java/team1/issue_tracker/user/UserService.java
@@ -1,6 +1,7 @@
 package team1.issue_tracker.user;
 
 import org.springframework.stereotype.Service;
+import team1.issue_tracker.user.dto.RegisterInfo;
 
 import java.util.Optional;
 
@@ -16,5 +17,21 @@ public class UserService {
     public String getNameById(String userId){
         Optional<User> byId = userRepository.findById(userId);
         return byId.get().getName();
+    }
+
+    public boolean isDuplicateId(String id) {
+        return userRepository.existsById(id);
+    }
+
+    public boolean isDuplicateNickName(String nickname) {
+        return userRepository.existsByName(nickname);
+    }
+
+    public void createUser(RegisterInfo registerInfo) {
+        if(isDuplicateId(registerInfo.id())) throw new IllegalArgumentException("이미 등록된 ID 입니다");
+        if(isDuplicateId(registerInfo.nickname())) throw new IllegalArgumentException("이미 등록된 닉네임 입니다");
+
+        User newUser = User.normalUSerOf(registerInfo);
+        userRepository.insert(newUser);
     }
 }
---
+++ be/issue_tracker/src/main/java/team1/issue_tracker/user/dto/RegisterInfo.java
@@ -0,0 +1,4 @@
+package team1.issue_tracker.user.dto;
+
+public record RegisterInfo(String id, String nickname, String password) {
+}
