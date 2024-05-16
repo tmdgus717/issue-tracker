defb89ada3e784dcbcbb44bfe4467d77766bea18
최수연 <tndus5383@gmail.com>
2024-05-16 16:28:16 +0900

    feat(#41): Milestone 기능 구현

--- be/issue_tracker/src/main/java/team1/issue_tracker/milestone/MilestoneController.java
+++ be/issue_tracker/src/main/java/team1/issue_tracker/milestone/MilestoneController.java
@@ -1,16 +1,23 @@
 package team1.issue_tracker.milestone;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team1.issue_tracker.milestone.dto.MilestoneListResponse;
+import team1.issue_tracker.milestone.dto.MilestoneMakeRequest;
 
 @RequestMapping("/milestone")
 @Slf4j
@@ -24,27 +31,34 @@ public class MilestoneController {
         this.milestoneService = milestoneService;
     }
 
-    // 마일스톤 전체 목록 조회
     @GetMapping("/list")
     public List<MilestoneListResponse> milestoneList() {
         log.debug("List of Milestones");
         return milestoneService.getList();
     }
 
-    // 마일스톤 생성
     @PostMapping
-    public void createMilestone() {
+    public ResponseEntity<String> createMilestone(@RequestBody MilestoneMakeRequest request) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
+        try {
+            LocalDateTime deadline = LocalDate.parse(request.getDeadline(), formatter).atStartOfDay();
+            milestoneService.createMilestone(request.getName(), request.getDescription(), deadline);
+            return ResponseEntity.ok("Milestone created successfully");
+        } catch (DateTimeParseException e) {
+            return ResponseEntity.badRequest().body("Invalid date format. Please use YYYY.MM.DD.");
+        }
     }
 
-    // 마일스톤 수정
     @PatchMapping("/{id}")
     public void updateMilestone(@PathVariable("id") Long id) {
         log.debug("Update Milestone.{}", id);
+        milestoneService.updateMilestone(id);
     }
 
-    // 마일스톤 삭제
     @DeleteMapping("/{id}")
     public void deleteMilestone(@PathVariable("id") Long id) {
         log.debug("Delete Milestone.{}", id);
+        milestoneService.deleteMilestone(id);
     }
 }
--- be/issue_tracker/src/main/java/team1/issue_tracker/milestone/MilestoneService.java
+++ be/issue_tracker/src/main/java/team1/issue_tracker/milestone/MilestoneService.java
@@ -1,12 +1,11 @@
 package team1.issue_tracker.milestone;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import team1.issue_tracker.Issue.Issue;
 import team1.issue_tracker.milestone.dto.MilestoneListResponse;
-import team1.issue_tracker.milestone.dto.MilestoneShowResponse;
-
-import java.util.List;
 
 @Service
 public class MilestoneService {
@@ -23,13 +22,17 @@ public class MilestoneService {
         return milestones.stream().map(MilestoneListResponse::of).toList();
     }
 
-    public MilestoneShowResponse getMilestoneAtIssue(Issue issue) {
-        Long milestoneId = issue.getMilestoneId();
-        if (milestoneId == null) {
-            return null;
-        }
-
-        Milestone milestone = milestoneRepository.findById(milestoneId).get();
-        return new MilestoneShowResponse(milestoneId, milestone.getName());
+    public void createMilestone(String name, String description, LocalDateTime deadline) {
+        Milestone milestone = Milestone.builder().name(name).description(description).deadline(deadline).build();
+        milestoneRepository.save(milestone);
     }
+
+    public void updateMilestone(Long id) {
+        Optional<Milestone> byId = milestoneRepository.findById(id);
+
+    }
+
+    public void deleteMilestone(Long id) {
+    }
+
 }
