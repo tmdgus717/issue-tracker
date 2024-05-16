34ba518acdf00b30a66dfd91d82e8c981accff6c
Miensoap <miensop51@gmail.com>
2024-05-16 14:01:13 +0900

    feat(#44) 댓글 추가 API 구현
    파일 첨부 미구현

--- be/issue_tracker/src/main/java/team1/issue_tracker/comment/CommentController.java
+++ be/issue_tracker/src/main/java/team1/issue_tracker/comment/CommentController.java
@@ -3,6 +3,7 @@ package team1.issue_tracker.comment;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
 import team1.issue_tracker.comment.dto.CommentListResponse;
+import team1.issue_tracker.comment.dto.CommentPostRequest;
 
 import java.util.List;
 
@@ -15,32 +16,32 @@ public class CommentController {
     }
 
     @GetMapping("/issue/{issueId}/comment/list")
-    public List<CommentListResponse> commentsAtIssue(@PathVariable long issueId){
+    public List<CommentListResponse> commentsAtIssue(@PathVariable long issueId) {
         return commentService.getCommentsAtIssue(issueId);
     }
 
     @PostMapping("/issue/{issueId}/comment")
-    public void writeComment(@PathVariable long issueId){
-
+    public void writeComment(@PathVariable long issueId, @RequestBody CommentPostRequest commentInfo, HttpServletRequest request) {
+        commentService.addComment(issueId, "test1", commentInfo); // 로그인 기능 구현 전 임시로 'test1' 유저 사용
     }
 
     @PatchMapping("/comment/{id}")
-    public Comment modifyComment(@PathVariable long id){
+    public Comment modifyComment(@PathVariable long id) {
         return null;
     }
 
     @DeleteMapping("/comment/{id}")
-    public void deleteComment(@PathVariable long id){
+    public void deleteComment(@PathVariable long id) {
 
     }
 
     @PostMapping("/comment/{id}/like")
-    public int likeComment(@PathVariable long id , HttpServletRequest request){
+    public int likeComment(@PathVariable long id, HttpServletRequest request) {
         return commentService.likeComment("test1", id); // 로그인 기능 구현 전 임시로 'test1' 유저 사용
     }
 
     @PostMapping("/comment/{id}/unlike")
-    public int unLikeComment(@PathVariable long id , HttpServletRequest request){
+    public int unLikeComment(@PathVariable long id, HttpServletRequest request) {
         return commentService.unlikeComment("test1", id);
     }
 }
