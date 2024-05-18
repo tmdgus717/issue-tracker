0d19ba7241a5e7b4446287aaba12c23dd886fafe
Miensoap <miensop51@gmail.com>
2024-05-18 21:48:19 +0900

    Test : 이슈 관련 인수 테스트 작성

--- be/issue_tracker/src/test/java/team1/issuetracker/acceptance/IssueTest.java
+++ be/issue_tracker/src/test/java/team1/issuetracker/acceptance/IssueTest.java
@@ -8,9 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ActiveProfiles;
 import team1.issuetracker.Issue.dto.IssueMakeRequest;
 
 import java.util.List;
@@ -22,17 +20,75 @@ public class IssueTest {
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 8080; // 테스트 프로파일에 맞는 포트 설정
+        RestAssured.port = 8080;
     }
 
     Request request = new Request();
 
-    @DisplayName("이슈를 등록하고 조회할 수 있다")
+    @DisplayName("이슈를 등록할 수 있다.")
     @Test
-    void test() {
+    void postIssue() {
+        // given
+        IssueMakeRequest issueMakeRequest = new IssueMakeRequest("이슈등록테스트용제목", "내용", List.of(), List.of(), null);
+
+
+        // when
+        ExtractableResponse<Response> response = request.post(issueMakeRequest, "/issue");
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.contentType()).isEqualTo(ContentType.JSON.toString());
+        assertThat(response.body().jsonPath().getString("title")).isEqualTo("이슈등록테스트용제목");
+    }
+
+    @DisplayName("이슈 등록시 존재하지 않는 라벨은 이슈에 추가할 수 없다.")
+    @Test
+    void postIssueWithNotExistsLabel() {
+        // given
+        IssueMakeRequest issueMakeRequest = new IssueMakeRequest("제목", "내용", List.of(), List.of(20L), null);
+
+        // when
+        ExtractableResponse<Response> response = request.post(issueMakeRequest, "/issue");
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("이슈 등록시 존재하지 않는 유저는 담당자로 추가 할 수 없다.")
+    @Test
+    void postIssueWithNotExistsUser() {
+        // given
+        IssueMakeRequest issueMakeRequest = new IssueMakeRequest("제목", "내용", List.of("noUser"), List.of(), null);
+
+        // when
+        ExtractableResponse<Response> response = request.post(issueMakeRequest, "/issue");
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("이슈 등록시 존재하지 않는 마일스톤은 이슈에 설정할 수 없다.")
+    @Test
+    void postIssueWithNotExistsMilestone() {
+        // given
+        IssueMakeRequest issueMakeRequest = new IssueMakeRequest("제목", "내용", List.of(), List.of(), 20L);
+
+        // when
+        ExtractableResponse<Response> response = request.post(issueMakeRequest, "/issue");
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("이슈목록을 조회할 수 있다")
+    @Test
+    void getIssueList() {
         // given
         IssueMakeRequest issueMakeRequest = new IssueMakeRequest("제목", "내용", List.of(), List.of(), null);
-        ExtractableResponse<Response> makeIssue = request.post(issueMakeRequest, "/issue");
+        request.post(issueMakeRequest, "/issue");
+        request.post(issueMakeRequest, "/issue");
+        request.post(issueMakeRequest, "/issue");
+        request.post(issueMakeRequest, "/issue");
 
         // when
         ExtractableResponse<Response> response = request.get("/issue/list");
