98df41276df6ac8f0c94343078f9eab44977d20a
sean <tmdgus717@naver.com>
2024-05-20 19:20:33 +0900

    feat(#64) LabelTest create,delete,list 구현

--- be/issue_tracker/src/main/java/team1/issuetracker/domain/label/dto/LabelMakeRequest.java
+++ be/issue_tracker/src/main/java/team1/issuetracker/domain/label/dto/LabelMakeRequest.java
@@ -1,7 +1,4 @@
 package team1.issuetracker.domain.label.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 public record LabelMakeRequest(String name, String description, String color) {
 }
