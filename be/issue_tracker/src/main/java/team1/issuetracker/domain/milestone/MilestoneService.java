0d11756e6b0747ceab9422043bde067b5345cd7e
최수연 <tndus5383@gmail.com>
2024-05-21 16:37:41 +0900

    feat(#47): milestone 권한 구현 완료

--- be/issue_tracker/src/main/java/team1/issuetracker/domain/milestone/MilestoneService.java
+++ be/issue_tracker/src/main/java/team1/issuetracker/domain/milestone/MilestoneService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team1.issuetracker.domain.Issue.Issue;
+import team1.issuetracker.domain.milestone.dto.MilestoneMakeRequest;
 import team1.issuetracker.domain.milestone.dto.MilestoneShowResponse;
 import team1.issuetracker.domain.milestone.dto.MilestoneListResponse;
 import team1.issuetracker.domain.user.auth.Authorizable;
@@ -37,28 +38,34 @@ public class MilestoneService implements Authorizable<Milestone, Long> {
         return new MilestoneShowResponse(milestoneId, milestone.getName());
     }
 
-    public void createMilestone(String name, String description, Date deadline) {
+    public Milestone createMilestone(String name, String description, Date deadline) {
         Milestone milestone = Milestone.builder().name(name).description(description).deadline(deadline).build();
-        milestoneRepository.save(milestone);
+        return milestoneRepository.save(milestone);
     }
 
-    public void deleteMilestone(Long id) {
-        Optional<Milestone> optionalMilestone = milestoneRepository.findById(id);
-        if (optionalMilestone.isEmpty()) {
-            throw new NoSuchElementException(id + "번 마일스톤이 존재하지 않습니다!");
-        }
-        Milestone milestone = optionalMilestone.get();
-        milestoneRepository.delete(milestone);
+    public void deleteMilestone(Long id, String userId) {
+        milestoneRepository.delete(authorize(id, userId));
     }
 
-    public void updateMilestone(String name, String description, Date deadline, Long id) {
-        Optional<Milestone> optionalMilestone = milestoneRepository.findById(id);
-        if (optionalMilestone.isEmpty()) {
-            throw new NoSuchElementException(id + "번 마일스톤이 존재하지 않습니다!");
-        }
+    public Milestone updateMilestone(MilestoneMakeRequest milestoneMakeRequest, Long id, String userId) {
+        Milestone origin = authorize(id, userId);
+        String name = milestoneMakeRequest.name();
+        String description = milestoneMakeRequest.description();
+        Date deadline = milestoneMakeRequest.deadline();
 
-        Milestone milestone = Milestone.builder().id(id).name(name).description(description).deadline(deadline).build();
-        milestoneRepository.save(milestone);
+        Milestone updateMilestone = Milestone.builder()
+                .id(origin.getId())
+                .name(name)
+                .description(description)
+                .issues(origin.getIssues())
+                .deadline(deadline)
+                .userId(userId)
+                .createdAt(origin.getCreatedAt())
+                .build();
+
+        milestoneRepository.save(updateMilestone);
+
+        return getMilestoneById(id);
     }
 
     @Override
@@ -67,14 +74,16 @@ public class MilestoneService implements Authorizable<Milestone, Long> {
         if (milestone.getUserId().equals(userId)) {
             throw new AuthorizeException(milestoneId + "번 마일스톤에 대한 권한이 없습니다.");
         }
+
         return milestone;
     }
 
     private Milestone getMilestoneById(long id) {
-        Optional<Milestone> byId = milestoneRepository.findById(id);
-        if (byId.isEmpty()) {
+        Optional<Milestone> optionalMilestone = milestoneRepository.findById(id);
+        if (optionalMilestone.isEmpty()) {
             throw new NoSuchElementException("존재하지 않는 마일스톤 입니다.");
         }
-        return byId.get();
+
+        return optionalMilestone.get();
     }
 }
