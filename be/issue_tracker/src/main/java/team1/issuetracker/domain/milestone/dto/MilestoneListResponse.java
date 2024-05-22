e7ae3847fbdf9e74222b8bf1fa7c2295c347a887
Miensoap <miensop51@gmail.com>
2024-05-22 18:00:28 +0900

    Chore : milestone list api 날짜 형식 수정

--- be/issue_tracker/src/main/java/team1/issuetracker/domain/milestone/dto/MilestoneListResponse.java
+++ be/issue_tracker/src/main/java/team1/issuetracker/domain/milestone/dto/MilestoneListResponse.java
@@ -1,10 +1,13 @@
 package team1.issuetracker.domain.milestone.dto;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import team1.issuetracker.domain.milestone.Milestone;
 
 
-public record MilestoneListResponse(long id, String name, String description, Date deadline, int openCounts,
+public record MilestoneListResponse(long id, String name, String description,
+                                    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd") Date deadline, int openCounts,
                                     int closedCounts, int completion) {
     public static MilestoneListResponse of(Milestone milestone) {
         return new MilestoneListResponse(milestone.getId(), milestone.getName(),
